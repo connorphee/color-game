bf5a384d76ca6c78fbafef474a37f242dc4e175d
Connor Phee <connor.phee@gmail.com>
2017-08-11 22:48:37 -0400

    Add ability to reset game, remove slow start. Remove var declarations in favor of let/const

--- app.js
+++ app.js
@@ -1,49 +1,44 @@
-var gameHeader = document.getElementById('gameHeader');
-var playDiv = document.getElementById('playDiv');
-var playButton = document.getElementById('playButton');
-var blocks = [];
-blocks[0] = document.getElementById('block1');
-blocks[1] = document.getElementById('block2');
-blocks[2] = document.getElementById('block3');
-blocks[3]= document.getElementById('block4');
-blocks[4] = document.getElementById('block5');
-blocks[5]= document.getElementById('block6');
-var answer;
-var isRightAnswer;
+const gameHeader = document.getElementById('gameHeader');
+const playDiv = document.getElementById('playDiv');
+const playButton = document.getElementById('playButton');
+const blocks = document.querySelectorAll('.colorBlock');
+let answer;
+let isRightAnswer;
 
-setTimeout(function () {
-	gameHeader.innerHTML = 'Ready to play?';
-	playDiv.style.visibility = 'visible';
-}, 2000);
-
-var startGame = function () {
+let startGame = function () {
 	setTimeout(function () {
 		assignColors();
 		pickAnswer();
 		gameHeader.innerHTML = 'Guess the color: ' + answer;
-	}, 500)
+		playButton.style.visibility = 'hidden';
+	}, 1000)
 }
 
-var endGame = function (selection) {
+let endGame = function (selection) {
 	isRightAnswer = checkAnswer(selection);
 	gameHeader.innerHTML = isRightAnswer ? 'YOU WIN' : 'YOU LOSE';
+	playButton.style.visibility = 'visible';
+	playButton.innerHTML = 'Restart';
+	playButton.onclick = startGame;
 }
 
-var pickAnswer = function () {
-	var num = Math.floor(Math.random() * 6);
+let pickAnswer = function () {
+	let num = Math.floor(Math.random() * 6);
 	answer = blocks[num].style.backgroundColor;
 }
 
-var assignColors = function () {
+let assignColors = function () {
 	blocks.forEach(function(block) {
 		block.style.backgroundColor = generateColor();
 	});
 }
 
-var checkAnswer = function (selection) {
+let checkAnswer = function (selection) {
 	return selection.style.backgroundColor === answer;
 }
 
-var generateColor = function () {
+let generateColor = function () {
 	return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
+
+startGame();
