54da283bc7d80062aebe617bccb47b4a969faeaf
Connor Phee <connor.phee@gmail.com>
2017-08-12 19:14:55 -0400

    Update single quotes to template literals. End game after first selection, rather than allowing each block to be clicked and change result. Break inner html editing into function due to operation being done in multiple places.

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-const gameHeader = document.getElementById('gameHeader');
-const playDiv = document.getElementById('playDiv');
-const playButton = document.getElementById('playButton');
-const blocks = document.querySelectorAll('.colorBlock');
+const gameHeader = document.getElementById(`gameHeader`);
+const playDiv = document.getElementById(`playDiv`);
+const playButton = document.getElementById(`playButton`);
+const blocks = document.querySelectorAll(`.colorBlock`);
 let answer;
 let isRightAnswer;
+let gameOver = false;
 
 let startGame = function () {
 	setTimeout(function () {
+		gameOver = false;
 		assignColors();
 		pickAnswer();
-		gameHeader.innerHTML = 'Guess the color: ' + answer;
-		playButton.style.visibility = 'hidden';
+		gameHeader.innerHTML = `Guess the color: ` + answer;
+		playButton.style.visibility = `hidden`;
 	}, 1000)
 }
 
 let endGame = function (selection) {
-	isRightAnswer = checkAnswer(selection);
-	gameHeader.innerHTML = isRightAnswer ? 'YOU WIN' : 'YOU LOSE';
-	playButton.style.visibility = 'visible';
-	playButton.innerHTML = 'Restart';
-	playButton.onclick = startGame;
+	if (!gameOver) {
+		isRightAnswer = checkAnswer(selection);
+		isRightAnswer ? updateInnerHtml(`YOU WIN`, gameHeader) : updateInnerHtml(`YOU LOSE`, gameHeader);
+		playButton.style.visibility = `visible`;
+		updateInnerHtml(`Restart`, playButton);
+		playButton.onclick = startGame;
+		gameOver = true;
+	}
 }
 
 let pickAnswer = function () {
 	let num = Math.floor(Math.random() * 6);
 	answer = blocks[num].style.backgroundColor;
+}
+
+let updateInnerHtml = function (text, objectToUpdate) {
+	objectToUpdate.innerHTML = text;
 }
 
 let assignColors = function () {
@@ -38,7 +47,7 @@ let checkAnswer = function (selection) {
 }
 
 let generateColor = function () {
-	return '#' + Math.floor(Math.random() * 16777215).toString(16);
+	return `#` + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 startGame();
